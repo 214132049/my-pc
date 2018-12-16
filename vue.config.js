2fc464427da005debdf433aa909a3449a7222108
DESKTOP-7GDUUR0\liwenli <214132049@qq.com>
2018-12-16 22:00:05 +0800

    添加预渲染配置

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+// vue.config.js
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: ['/', '/about']
+      })
+    ]
+  }
+}
