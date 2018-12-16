9360c43a5f21d2c9af605beea914e01ed4c5337c
DESKTOP-7GDUUR0\liwenli <214132049@qq.com>
2018-12-16 22:23:34 +0800

    修改预渲染配置

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,19 @@ module.exports = {
         // Required - The path to the webpack-outputted app to prerender.
         staticDir: path.join(__dirname, 'dist'),
         // Required - Routes to render.
-        routes: ['/', '/about']
+        routes: ['/', '/about'],
+        postProcess: function (context) {
+          var titles = {
+            '/': '首页',
+            '/about': '关于我们',
+            '/contact': '联系我们'
+          }
+          context.html = context.html.replace(
+            /<title>[^<]*<\/title>/i,
+            '<title>' + titles[context.route] + '</title>'
+          )
+          return context
+        }
       })
     ]
   }
